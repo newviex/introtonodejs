c12386d319fdf2da39710b02c0600b7c1bd9e7fa
newviex <xaldrienallanigue@gmail.com>
2023-04-09 20:41:48 +0800

    add supervillainslist and names

--- introToNode.js
+++ introToNode.js
@@ -1,12 +1,16 @@
 // const fs = require('fs');
 
 // fs.copyFileSync("source.txt","destination.txt");
-
+const supervillains = require('supervillains');
 var superheroes = require('superheroes');
 
+var superVillains = supervillains.random();
+var superVillains = supervillains.all;
 var superHeroList = superheroes.all;
 
 var superHeroesNames = superheroes.random();
 
+
 console.log(superHeroesNames);
 console.log(superHeroList);
+console.log(superVillains);
