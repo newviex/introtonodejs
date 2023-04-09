9054d52210d354eb242765022c92b3d7cc356793
newviex <xaldrienallanigue@gmail.com>
2023-04-09 20:32:29 +0800

    display all heroes name

--- introToNode.js
+++ introToNode.js
@@ -1,3 +1,12 @@
 // const fs = require('fs');
 
 // fs.copyFileSync("source.txt","destination.txt");
+
+var superheroes = require('superheroes');
+
+var superHeroList = superheroes.all;
+
+var superHeroesNames = superheroes.random();
+
+console.log(superHeroesNames);
+console.log(superHeroList);
